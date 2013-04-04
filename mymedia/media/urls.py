ad34c3ca595080db540a79a1420f46f8ac460d18
Andrew Little <aalittle@gmail.com>
2013-04-03 22:21:25 -0300

    using generic class based views to simplify the REST view logic

--- mymedia/media/urls.py
+++ mymedia/media/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, url
 from rest_framework.urlpatterns import format_suffix_patterns
+from media import views
 
-urlpatterns = patterns('media.views',
-    url(r'^media/$', 'medium_list'),
-    url(r'^media/(?P<pk>[0-9]+)$', 'medium_detail'),
+urlpatterns = patterns('',
+    url(r'^media/$', views.MediumList.as_view()),
+    url(r'^media/(?P<pk>[0-9]+)/$', views.MediumDetail.as_view()),
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
